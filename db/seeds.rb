07e04188759e262480ff117e81201f1beb3e07bb
Alex Godfrey <alexgodfrey2@gmail.com>
2022-04-14 19:35:27 +0200

    commiting my changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning up database'
+Restaurant.destroy_all
+puts 'my database is clean'
+
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: 'Italian'
+  )
+  puts "restaurant #{restaurant_id} is created"
+end
+
+puts 'done'
